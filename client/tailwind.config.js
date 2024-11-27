fa651e75be0fef72c2ccf14fa853c66d09d07065
Ramontaffa <taffarelramon1@gmail.com>
2024-11-26 23:57:00 -0300

    Added CreateButton component

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -20,6 +20,7 @@ module.exports = {
         sm: "calc(var(--radius) - 4px)",
       },
       colors: {
+        green: "hsl(var(--green))",
         gray84: "#d6d6d6",
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
