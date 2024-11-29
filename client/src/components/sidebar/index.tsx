e758cb3bbb96d0c6b85fb33de54b5301843d71e3
iianmelo <imm2@cin.ufpe.br>
2024-11-29 10:06:21 -0300

    Fixing style

--- client/src/components/sidebar/index.tsx
+++ client/src/components/sidebar/index.tsx
@@ -7,6 +7,7 @@ import GameControllerOn from "../../assets/GameControllerOn.png";
 import UserOff from "../../assets/UserOff.png";
 import UserOn from "../../assets/UserOn.png";
 import  CITi_logo  from "../../assets/CITi_white_logo.png";
+import { Button } from '../ui/button';
 
 export default function Sidebar() {
   const [ExploreMatchesSelected, setExploreMatchesSelected] = useState(false);
@@ -32,14 +33,14 @@ return (
       <Image src={CITi_logo} alt="Logo citi" width={100} height={300} />
     </div>
 
-    <div style={{display: 'flex', flexDirection: 'column', gap: '10px', width: '100%', marginTop: '40px'}}>
-      <button className="custom-button" onClick={() => handleClick(1)}>
-        <Image src={ExploreMatchesSelected ? GameControllerOn : GameControllerOff} alt="Explore Matches" width={30} height={30}/>
-        <p style={{ color: 'white' }}>Explorar Partidas</p>
+    <div style={{display: 'flex', flexDirection: 'column', gap: '10px', width: '100%', marginTop: '40px',}}>
+      <button className="custom-button h-10" onClick={() => handleClick(1)}>
+        <Image className='ml-3' src={ExploreMatchesSelected ? GameControllerOn : GameControllerOff} alt="Explore Matches" width={30} height={30}/>
+        <p style={{ fontWeight:'700', color: 'white' }}>Explorar Partidas</p>
       </button>
 
-      <button className="custom-button" onClick={() => handleClick(2)}>
-        <Image src={ProfileSelected ? UserOn : UserOff} alt="Profile" width={30} height={30}/>
+      <button className="custom-button h-10" onClick={() => handleClick(2)}>
+        <Image className='ml-3' src={ProfileSelected ? UserOn : UserOff} alt="Profile" width={30} height={30}/>
         <p style={{color: 'white'}}>Perfil</p>
       </button>
     </div>
