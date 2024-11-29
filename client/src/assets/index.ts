c59d122cc6ecb2de947361e1aafd68c1d5f1bc4d
Ian Melo <imm2@cin.ufpe.br>
2024-11-29 11:32:46 -0300

    Fixing assets importations issues and localization of the topbar folder.

--- client/src/assets/index.ts
+++ client/src/assets/index.ts
@@ -1,3 +1,7 @@
 import LogoCITi from "./Logo.png";
+import GameControllerOn from "./GameControllerOn.png";
+import GameControllerOff from "./GameControllerOff.png";
+import UserOn from "./UserOn.png";
+import UserOff from "./UserOff.png";
 
-export { LogoCITi };
+export { LogoCITi, GameControllerOn, GameControllerOff, UserOn, UserOff };
