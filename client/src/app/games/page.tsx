53bce81d8d7e624fca70d0eae0f64528fb23ea73
Ian Melo <imm2@cin.ufpe.br>
2024-12-05 19:26:09 -0300

    updating games

--- client/src/app/games/page.tsx
+++ client/src/app/games/page.tsx
@@ -1,57 +1,180 @@
-"use client";
-
+import Sidebar from "@/components/sidebar";
 import TopBar from "@/components/topbar";
 import * as React from "react";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 import CardTeste from "@/components/Card/indext";
 import CreateButton from "@/components/CreateButton";
 
 export default function Games() {
-  const [games, setGames] = React.useState([
+  const games = [
     {
-      title: "Game 1",
-      platform: "PC",
-      date: "2023-10-01",
+      title: "League of Legends",
+      platform: "Discord",
+      date: "2024-12-02",
+      time: "20:00",
+      currentPlayers: 4,
+      maxPlayers: 10,
+    },
+    {
+      title: "Valorant",
+      platform: "Teamspeak",
+      date: "2024-12-03",
+      time: "22:00",
+      currentPlayers: 8,
+      maxPlayers: 12,
+    },
+    {
+      title: "Counter-Strike",
+      platform: "Steam",
+      date: "2024-12-05",
       time: "18:00",
       currentPlayers: 5,
       maxPlayers: 10,
     },
-  ]);
+  {
+    title: "Fortnite",
+    platform: "Epic Games",
+    date: "2024-12-06",
+    time: "19:00",
+    currentPlayers: 6,
+    maxPlayers: 16,
+  },
+  {
+    title: "Apex Legends",
+    platform: "Origin",
+    date: "2024-12-07",
+    time: "21:00",
+    currentPlayers: 3,
+    maxPlayers: 20,
+  },
+  {
+    title: "Dota 2",
+    platform: "Steam",
+    date: "2024-12-08",
+    time: "17:00",
+    currentPlayers: 7,
+    maxPlayers: 10,
+  },
+  {
+    title: "Overwatch",
+    platform: "Battle.net",
+    date: "2024-12-09",
+    time: "20:30",
+    currentPlayers: 5,
+    maxPlayers: 12,
+  },
+  {
+    title: "Overwatch",
+    platform: "Battle.net",
+    date: "2024-12-09",
+    time: "20:30",
+    currentPlayers: 5,
+    maxPlayers: 12,
+  },
+  {
+    title: "Overwatch",
+    platform: "Battle.net",
+    date: "2024-12-09",
+    time: "20:30",
+    currentPlayers: 5,
+    maxPlayers: 12,
+  },
+  {
+    title: "Overwatch",
+    platform: "Battle.net",
+    date: "2024-12-09",
+    time: "20:30",
+    currentPlayers: 5,
+    maxPlayers: 12,
+  },
+  {
+    title: "Overwatch",
+    platform: "Battle.net",
+    date: "2024-12-09",
+    time: "20:30",
+    currentPlayers: 5,
+    maxPlayers: 12,
+  },
+  {
+    title: "Overwatch",
+    platform: "Battle.net",
+    date: "2024-12-09",
+    time: "20:30",
+    currentPlayers: 5,
+    maxPlayers: 12,
+  },
+  {
+    title: "Overwatch",
+    platform: "Battle.net",
+    date: "2024-12-09",
+    time: "20:30",
+    currentPlayers: 5,
+    maxPlayers: 12,
+  },
+  {
+    title: "Overwatch",
+    platform: "Battle.net",
+    date: "2024-12-09",
+    time: "20:30",
+    currentPlayers: 5,
+    maxPlayers: 12,
+  },
+  {
+    title: "Overwatch",
+    platform: "Battle.net",
+    date: "2024-12-09",
+    time: "20:30",
+    currentPlayers: 5,
+    maxPlayers: 12,
+  },
+  {
+    title: "Overwatch",
+    platform: "Battle.net",
+    date: "2024-12-09",
+    time: "20:30",
+    currentPlayers: 5,
+    maxPlayers: 12,
+  }
+  ];
 
   return (
-    <div className="flex flex-col h-screen w-screen bg-loginBg">
-      <TopBar isReturnEnabled={false} />
+    <div className="flex flex-row">
+      <Sidebar />
+      <div className="flex flex-col h-screen w-screen bg-loginBg">
+        <TopBar isReturnEnabled={false} />
 
-      <div className="flex flex-col flex-grow">
-        <h1 className="font-barlow font-normal text-[1.75rem] leading-9 pt-12 ml-[4.5rem] w-fit">
-          Próximas partidas
-        </h1>
+        <div className="flex flex-col flex-grow h-[calc(100vh-152px)]">
+          <h1 className="font-barlow font-normal text-[1.75rem] leading-9 pt-12 ml-[4.5rem] w-fit">
+            Próximas partidas
+          </h1>
 
-        <ScrollArea className="mt-14 mx-[4.5rem] border-2 border-black ">
-          {games.length > 0 ? (
-        <div className="flex flex-wrap gap-8 p-2">
-          {games.map((game, index) => (
-            <CardTeste
-          key={index}
-          title={game.title}
-          platform={game.platform}
-          date={game.date}
-          time={game.time}
-          currentPlayers={game.currentPlayers}
-          maxPlayers={game.maxPlayers}
-            />
-          ))}
-        </div>
-          ) : (
-        <div className="flex justify-center items-center h-40">
-          <p className="text-lg text-gray-600">Não há partidas</p>
-        </div>
-          )}
-        </ScrollArea>
+          <ScrollArea className="h-full mx-12 rounded-md p-0 mb-8">
+            {games.length > 0 ? (
+                <div className="grid grid-cols-[repeat(auto-fit,minmax(230px,1fr))] gap-4 p-2">
+                {games.map((game, index) => (
+                  <CardTeste
+                  key={index}
+                  title={game.title}
+                  platform={game.platform}
+                  date={game.date}
+                  time={game.time}
+                  currentPlayers={game.currentPlayers}
+                  maxPlayers={game.maxPlayers}
+                  />
+                ))}
+                </div>
+            ) : (
+              <div className="flex justify-center items-center h-40">
+                <p className="text-2xl text-gray-500">Não há partidas no momento</p>
+              </div>
+            )}
+            <ScrollBar orientation="vertical" />
+          </ScrollArea>
 
-        <div className="ml-auto mx-[4.5rem] mt-8">
-          <CreateButton />
         </div>
+          <div className="ml-auto mx-[4.5rem] mb-12">
+            <CreateButton />
+          </div>
       </div>
     </div>
   );
