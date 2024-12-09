76679566633d02acaa45618896e9a6676ef0431e
Ian Melo <imm2@cin.ufpe.br>
2024-12-09 14:14:33 -0300

    correcting import error

--- client/src/app/detailspage/page.tsx
+++ client/src/app/detailspage/page.tsx
@@ -1,14 +1,11 @@
-import Image from "next/image";
-
 import Sidebar from "@/components/sidebar";
 
 import Topbar from "@/components/topbar";
-
+import { ScrollArea,ScrollBar } from "@/components/ui/scroll-area";
 import MatchesButton from "@/components/Button";
 
 import React from "react";
-
-import { UserCircle } from "@/assets";
+import { CircleUserRound } from "lucide-react";
 
 interface DetailsPageProps {
   title: string;
@@ -21,7 +18,7 @@ interface DetailsPageProps {
   players: string[];
 }
 
-export const testvalues: DetailsPageProps = {
+const testvalues: DetailsPageProps = {
   title: "Minecraft",
   platform: "Discord",
   date: "06/07/2023",
@@ -54,18 +51,16 @@ export const testvalues: DetailsPageProps = {
   ],
 };
 
-const DetailsPage: React.FC<DetailsPageProps> = (props) => {
-  const {
-    title = testvalues.title,
-    platform = testvalues.platform,
-    date = testvalues.date,
-    time = testvalues.time,
-    link = testvalues.link,
-    description = testvalues.description,
-    maxPlayers = testvalues.maxPlayers,
-    players = testvalues.players,
-  } = props;
-
+function DetailsPage({
+  title,
+  platform,
+  date,
+  time,
+  link,
+  description,
+  maxPlayers,
+  players,
+}: DetailsPageProps) {
   const currentPlayers = players.length;
 
   const roomStatus: "available" | "full" | "inside" =
@@ -75,72 +70,78 @@ const DetailsPage: React.FC<DetailsPageProps> = (props) => {
       ? "full"
       : "inside";
 
+  const bgColors = {
+    available: "bg-[#D2EFFE]",
+    full: "bg-[#D5C6FA]",
+    inside: "bg-[##D0F4E4]"
+  };
+
+
   return (
-    <div className="flex h-dvh overflow-y-auto bg-[#F5F5F5]">
+    <div className="flex h-screen overflow-y-auto">
       <Sidebar />
-      <div className="flex-col flex-grow ">
+      <div className="flex flex-col flex-grow overflow-y-auto h-full bg-[#F5F5F5]">
         <Topbar isReturnEnabled={true} />
-        <div className="flex flex-grow justify-center bg-[#F5F5F5] pr-[40px] pl-16 py-[20px]">
-          <div className="flex flex-col flex-grow max-w-md pr-8">
+        <div className="flex flex-col gap-4 gdcustom:gap-0 gdcustom:flex-row flex-grow items-center gdcustom:items-start justify-center bg-[#F5F5F5] pr-[40px] pl-16 py-[10vh]">
+          <div className="flex flex-col items-center gdcustom:items-start flex-grow max-w-md w-full gdcustom:pr-8">
             <h1 className="text-[#6800E4] font-barlow text-[14px] font-normal leading-[15px] text-left">
               Partidas
             </h1>
             <h1 className="text-[#000000] font-barlow text-[28px] font-medium leading-[36px] text-left">
               {title}
             </h1>
-            <p className="text-lg font-barlow text-[#454545]">{date} | {time}</p>
+            <p className="text-[16px] leading-[19.2px] font-barlow text-[#454545]">
+              {date} | {time}
+            </p>
             <p className="font-barlow text-[16px] font-normal leading-[19.2px] text-left py-2">
               {platform}
             </p>
             <div className="py-2">
-              <p className="text-[#000000] font-medium  font-barlow">
+              <p className="text-[16px] leading-[19.2px] text-[#000000] font-medium font-barlow">
                 Descrição:
               </p>
-              <p className="text-[14px] leading-[17px] font-barlow bg-[#FFFFFF80] text-[#454545] rounded-lg p-2 shadow-md">
+              <p className="text-[14px] leading-[16.8px] font-barlow bg-[#FFFFFF80] text-[#454545] rounded-lg p-2 shadow-md ">
                 {description}
               </p>
-              <p className="text-[#000000] font-barlow font-medium pt-2 text-[16px] leading-[19.2px]">
+              <p className="text-[16px] leading-[19.2px] text-[#000000] font-barlow font-medium pt-2">
                 Link:
               </p>
-              <p className="text-[14px] leading-[17px] font-barlow bg-[#FFFFFF80] text-[#454545] rounded-lg p-2 shadow-md">
+              <p className="text-[14px] leading-[16.8px] font-barlow bg-[#FFFFFF80] text-[#454545] rounded-lg p-2 shadow-md">
                 {link}
               </p>
-              <div className="mt-6 flex justify-center">
-                <MatchesButton roomStatus={roomStatus} />
+              <div className="mt-6 flex justify-center items-center">
               </div>
             </div>
           </div>
 
-          {/* Section for Participants */}
-          <div className="flex flex-col items-start p-4 text-lg font-barlow bg-[#F5F5F5] shadow-md w-[400px]  rounded-2xl">
-            <div className="flex items-center justify-between w-full mb-4 ">
-              <p className=" font-barlow text-[#000000] text-[24px] leading-[32px]">Participantes</p>
-              <p className=" font-barlow text-[#000000]">
+          <div className="flex flex-col items-center gdcustom:items-start p-4 text-lg font-barlow bg-[#F5F5F5] shadow-md gdcustom:w-[400px] min-w-[250px] rounded-2xl">
+            <div className="flex items-center justify-between min-w-[10px] w-full mb-4">
+              <p className="text-[24px] leading-[32px] font-barlow text-[#000000]">
+                Participantes
+              </p>
+              <p className="text-[24px] leading-[32px] font-barlow text-[#000000]">
                 {currentPlayers} / {maxPlayers}
               </p>
             </div>
 
-            <div className=" h-[350px] bg-[#D2EFFE] p-6 w-full overflow-y-auto  rounded-2xl">
-              <div className="text-[16px] text-[#454545] font-barlow leading-[19.2px]">
+            <ScrollArea className={`mt-4 h-[500px] ${bgColors[roomStatus]} p-5 w-full overflow-y-auto rounded-2xl`}>
+              <div className="text-[16px] leading-[19.2px] text-[#454545] p-1 font-barlow">
                 {players.map((player, index) => (
                   <div key={index} className="flex items-center gap-2 mb-2">
-                    <Image
-                      src={UserCircle}
-                      alt="User On Icon"
-                      width={20}
-                      height={20}
-                    />
-                    <span>{player}</span>
+                  <CircleUserRound size={24} />
+                  <span>{player}</span>
                   </div>
                 ))}
               </div>
-            </div>
+              <ScrollBar orientation="vertical"/>
+            </ScrollArea>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
-export default DetailsPage;
-
+export default function Page() {
+  return <DetailsPage {...testvalues} />;
+}
